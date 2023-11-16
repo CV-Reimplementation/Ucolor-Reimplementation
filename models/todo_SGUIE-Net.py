f0513196f5d21fc9ff905d512c6ae95e7f348391
xuhangc <cxhhg11@gmail.com>
2023-11-16 09:52:30 +0800

    add

---
+++ models/todo_SGUIE-Net.py
@@ -0,0 +1,257 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+##############################################################################
+# Classes
+##############################################################################
+
+def conv(in_channels, out_channels, kernel_size, bias=False, stride=1):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size // 2), bias=bias, stride=stride)
+
+
+## Channel Attention Layer
+class CALayer(nn.Module):
+    def __init__(self, channel, reduction=8, bias=True):
+        super(CALayer, self).__init__()
+        # global average pooling: feature --> point
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # feature channel downscale and upscale --> channel weight
+        self.conv_du = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=bias),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, padding=0, bias=bias),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv_du(y)
+        return x * y
+
+
+## Pixel Attention Layer
+class PALayer(nn.Module):
+    def __init__(self, channel, reduction=8, bias=True):
+        super(PALayer, self).__init__()
+        self.pa = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=bias),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, 1, 1, padding=0, bias=bias),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.pa(x)
+        return x * y
+
+
+## Features Attention include Channel attention and Pixel Attention
+class FABlock(nn.Module):
+    def __init__(self, dim, kernel_size, ):
+        super(FABlock, self).__init__()
+        self.conv1 = conv(dim, dim, kernel_size, bias=True)
+        self.act1 = nn.ReLU(inplace=True)
+        self.conv2 = conv(dim, dim, kernel_size, bias=True)
+        self.calayer = CALayer(dim)
+        self.palayer = PALayer(dim)
+
+    def forward(self, x):
+        res = self.act1(self.conv1(x))
+        res = res + x
+        res = self.conv2(res)
+        res = self.calayer(res)
+        res = self.palayer(res)
+        res = res + x
+        return res
+
+
+## Channel Attention Block(CAB)
+class CAB(nn.Module):
+    def __init__(self, n_feat, kernel_size, reduction, bias, act):
+        super(CAB, self).__init__()
+        modules_body = []
+        modules_body.append(conv(n_feat, n_feat, kernel_size, bias=bias))
+        modules_body.append(act)
+        modules_body.append(conv(n_feat, n_feat, kernel_size, bias=bias))
+
+        self.CA = CALayer(n_feat, reduction, bias=bias)
+        self.body = nn.Sequential(*modules_body)
+
+    def forward(self, x):
+        res = self.body(x)
+        res = self.CA(res)
+        res = res + x
+        return res
+
+
+# Attention augmented feature
+class FA(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, dim=64, kernel_size=3):
+        super(FA, self).__init__()
+        self.conv1 = conv(in_channels, dim, kernel_size)
+        self.fab = FABlock(dim, kernel_size)
+        self.unet = UNet(dim, dim)
+        self.conv2 = conv(dim, out_channels, kernel_size)
+
+    def forward(self, raw):
+        x = self.conv1(raw)
+        x1 = self.fab(x)
+        x2 = self.unet(x1)
+        x3 = self.conv2(x2.clone())
+        img = x3 + raw
+        return x2, img
+
+
+class Group(nn.Module):
+    def __init__(self, dim, kernel_size, blocks):
+        super(Group, self).__init__()
+        modules = [FABlock(dim, kernel_size) for _ in range(blocks)]
+        modules.append(conv(dim, dim, kernel_size))
+        self.gp = nn.Sequential(*modules)
+
+    def forward(self, x):
+        res = self.gp(x)
+        res = res + x
+        return res
+
+
+#############################################
+# Unet
+#############################################
+
+# Unet parts
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels=11, n_classes=3, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        # x = torch.nn.Sigmoid()(x)
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        x = torch.nn.Tanh()(x)
+        # print(x.shape)
+        return x
+
+
+class SGUIENet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, dim=64, kernel_size=3, blocks=4, groups=3):
+        super(SGUIENet, self).__init__()
+        self.conv1 = conv(in_channels, dim, kernel_size)
+        self.fab = FABlock(dim, kernel_size)
+        self.orb = nn.Sequential(*[Group(dim, kernel_size, blocks) for _ in range(groups)])
+        self.conv2 = conv(dim, out_channels, kernel_size)
+
+    def forward(self, raw):
+        # main branch
+        x1 = self.conv1(raw)
+        x1 = self.fab(x1)
+
+        x1 = self.orb(x1)
+        x1 = self.conv2(x1)
+        x1 += raw
+        x1 = torch.tanh(x1)
+
+        return x1
+    
+if __name__ == '__main__':
+    t = torch.randn(1, 3, 256, 256).cuda()
+    model = SGUIENet().cuda()
+    res = model(t)
+    print(res.shape)
